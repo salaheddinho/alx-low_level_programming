366c6179931a28f05833dfc1a59348ac5f263d16
salaheddinho <salahhabibi777@gmail.com>
2023-08-11 04:19:08 -0700

    4-print_alphabt.c

--- 4-print_alphabt.c
+++ 4-print_alphabt.c
@@ -1,25 +1,22 @@
+alphabt.c
+241 B
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  *
  * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
 	char c;
-
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		if ((c == 'q' || c == 'e') != 1)
 		{
 			putchar(c);
 		}
-
 	}
 	putchar('\n');
 	return (0);
 }
-
