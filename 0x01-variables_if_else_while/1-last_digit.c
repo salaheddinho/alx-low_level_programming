6ef1731db1d2cde2eb3742c6ab6e5f3c974c5cb2
salaheddinho <salahhabibi777@gmail.com>
2023-08-11 03:33:07 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,3 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Brennan D Baraban
- */
 
 #include <stdlib.h>
 #include <time.h>
@@ -28,4 +24,4 @@ int main(void)
 		printf("%d is zero\n", n);
 
 	return (0);
-}}
+}
