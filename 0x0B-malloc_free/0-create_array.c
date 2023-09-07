de75248eee50b24d2abcfd693ef67a8072d4dec8
salaheddinho <salahhabibi777@gmail.com>
2023-09-06 18:24:27 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,52 +1,30 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- *  * create_array - creates an array of chars,
- *   * and initializes it with a specific char.
- *    * @size: the size of the array
- *     * @c: initial value
- *      *
- *       * Return: a pointer to the array, or NULL if it fails
+ *  * create_array - creates an array of chars and initializes
+ *   *                it with a specific char.
+ *    *
+ *     * @size: array size
+ *      * @c: char to initialize array with
+ *       *
+ *        * Return: array @a
  *        */
+
 char *create_array(unsigned int size, char c)
 {
-		char *array;
-			unsigned int i;
+		unsigned int i;
+			char *a;
 
 				if (size == 0)
 							return (NULL);
 
-					array = (char *)malloc(sizeof(char) * size);
-						if (array == NULL)
+					a = malloc(size * sizeof(char));
+
+						if (a == NULL)
 									return (NULL);
 
 							for (i = 0; i < size; i++)
-										array[i] = c;
+										a[i] = c;
 
-								return (array);
-}
- *  * create_array - creates an array of chars,
- *   * and initializes it with a specific char.
- *    * @size: the size of the array
- *     * @c: initial value
- *      *
- *       * Return: a pointer to the array, or NULL if it fails
- *        
-char *create_array(unsigned int size, char c)
-{
-		char *array;
-			unsigned int i;
-
-				if (size == 0)
-							return (NULL);
-
-					array = (char *)malloc(sizeof(char) * size);
-						if (array == NULL)
-									return (NULL);
-
-							for (i = 0; i < size; i++)
-										array[i] = c;
-
-								return (array);
+								return (a);
 }
