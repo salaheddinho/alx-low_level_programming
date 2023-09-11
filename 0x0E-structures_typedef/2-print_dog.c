b65c7f67b4342be7caac49e7528bbc74847b24c8
salaheddinho <salahhabibi777@gmail.com>
2023-09-11 15:03:28 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,22 @@
-#include "dog.h"
+#include <stdio.h>
 
-/**
-* print_dog - prints a struct dog
-*@d: dog identification
-*
-*/
+struct Dog {
+    char name[20];
+    int age;
+    char breed[20];
+};
 
-void print_dog(struct dog *d)
-{
-if (d == NULL )
-return 0; 
-
-if ((*d).name == NULL)
-printf("nil\n");
-else
-printf("Name: %s\n", d->name);
-
-if ((*d).age < 0)
-printf("nil\n");
-else
-printf("Age: %f\n", d->age);
-
-if ((*d).owner == NULL)
-printf("nil\n");
-else
-printf("Owner: %s\n", d->owner);
-
+int main() {
+    struct Dog myDog;
+    myDog.age = 3;
+    
+    // Initialize name and breed without trailing whitespace
+    strcpy(myDog.name, "Buddy");
+    strcpy(myDog.breed, "Golden Retriever");
+    
+    printf("Name: %s\n", myDog.name);
+    printf("Age: %d\n", myDog.age);
+    printf("Breed: %s\n", myDog.breed);
+    
+    return 0;
 }
