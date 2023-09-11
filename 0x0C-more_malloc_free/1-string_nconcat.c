abcf2805dbd89fa62df6d7974fe3a2daa6676422
salaheddinho <salahhabibi777@gmail.com>
2023-09-11 14:42:22 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,44 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *  * string_nconcat - function that concatenates two strings
- *   *
- *    * @s1: string 1
- *     * @s2: string 2
- *      * @n: bytes of @s2 to add to @s1 to be a new string
- *       *
- *        * Return: new string followed by the first @n bytes
- *         *         of string 2 @s2 or NULL
- *         */
+*string_nconcat - Concatenates two strings using at
+*most an inputted number of bytes.
+*@s1: The first string.
+*@s2: The second string.
+*@n: The maximum number of bytes of s2 to concatenate to s1.
+*
+*Return: If the function fails - NULL.
+*Otherwise - a pointer to the concatenated space in memory.
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-		unsigned int l1, l2, i, j;
-			char *str;
+	char *concat;
+	unsigned int len = n, index;
 
-				/*treat NULL as empty string*/
-				if (s1 == NULL)
-							s1 = "";
-					if (s2 == NULL)
-								s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-						l1 = l2 = 0;
-							/*length of both s1 & s2 respectively*/
-							while (s1[l1] != '\0')
-										l1++;
-								while (s2[l2] != '\0')
-											l2++;
+	if (s2 == NULL)
+		s2 = "";
 
-									/*set n to length of s2*/
-									if (n >= l2)
-												n = l2;
+	for (index = 0; s1[index]; index++)
+		len++;
 
-										str = (char *) malloc((l1 + n + 1) * sizeof(char));
-											if (str == NULL)
-														return (NULL);
+	concat = malloc(sizeof(char) * (len + 1));
 
-												/*add s1 to str*/
-												for (i = 0; s1[i] != '\0'; i++)
-															str[i] = s1[i];
-													/*add s2 n bytes to str*/
-													for (j = 0; j < n && s2[j] != '\0'; j++)
-															{
-																		str[i] = s2[j];
-																				i++;
-																					}
-														str[i] = '\0';
-															return (str);
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
